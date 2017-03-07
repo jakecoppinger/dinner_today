dcee018ea3a52b8805184ca3838b2733134ff7c2
Jake Coppinger <jake@jakecoppinger.com>
2017-03-07 18:35:45 +1100

    Parsed dinner data

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
- 
+
 import csv
-import sys
-import pprint
+from pprint import pprint
 import json
 
+json_filename = "dinner_data.json"
+
+dinners = {}
+
+for week in range(1,9):
+    reader = csv.reader(open('data/week' + str(week) + '.csv'))
+
+    result = {}
+    for row in reader:
+        key = row[0].strip()
+        meals = [meal.strip() for meal in row[1:]]
+        assert(len(meals) == 7)
+        result[key] = meals
+
+    result.pop("Breakfast")
+    result.pop("")
+
+    dinners[str(week)] = result
 
 
-fieldnames = ['date','delta','vendor','balance']
-reader = csv.DictReader(open(variables_file, 'rb'),fieldnames=fieldnames)
-dict_list = []
-for line in reader:
-    dict_list.append(line)
-dict_list.reverse()
-return dict_list
+pprint(dinners)
+   
+with open(json_filename, 'w') as outfile:
+    outfile.write(json.dumps(dinners, sort_keys=True, 
+        indent=4, separators=(',', ': ')))
 
-    
+print("///")
+print("Data written to " + json_filename)
