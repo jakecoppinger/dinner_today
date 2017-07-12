ed69d53e3a7026c12ca796433217b90300b13e5b
Jake Coppinger <jake@jakecoppinger.com>
2017-07-12 22:07:07 +1000

    Full working prototype with refactored JS

--- script.js
+++ script.js
@@ -84,9 +84,6 @@ function generateHumanDate() {
     return getOrdinal(dayOfMonth) + " of " + monthNames[d.getMonth()]
 }
 
-function getHumanDate() {
-    document.write(generateHumanDate());
-}
 
 // Write str into DOM object with class className
 function writeToClass(str, className) {
@@ -101,11 +98,14 @@ function menuExistsForDate(data, d) {
     return false; 
 }
 
-
-/////////////
-
+function prettyPrintDate(d) {
+    console.log("Date " + d + ":");
+    console.log("Day: " + d.getDayMondayBased());
+    console.log("Week number: " + d.getWeek()); 
+}
 
 Date.prototype.getDayMondayBased = function() {
+    var d = new Date(this.getTime());
     var x = d.getDay() - 1;
     if(x == -1) {
         return 6;
@@ -113,25 +113,11 @@ Date.prototype.getDayMondayBased = function() {
     return x;
 }
 
-//var d = new Date(); 
-
 function parseISOLocal(s) {
   var b = s.split(/\D/);
   return new Date(b[0], b[1]-1, b[2], b[3], b[4], b[5]);
 }
 
-var d = parseISOLocal('2017-07-24T19:40:00');
-
-console.log("Date:");
-console.log(d);
-console.log("Day: " + d.getDayMondayBased());
-console.log("Week number: " + d.getWeek());
-
-
-var day = d.getDayMondayBased();
-
-
-var menuExists = menuExistsForDate(data,d);
 
 
 function mealsForDate(data,d) {
@@ -139,39 +125,81 @@ function mealsForDate(data,d) {
         return undefined;
     }
 
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const days = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+          'Sunday'
+    ];
 
-    var dayName = days[day];
+    var dayName = days[d.getDayMondayBased()];
+    console.log(d);
+    console.log(d.getDayMondayBased());
+    console.log("\n");
     return data[d.getFullYear()][d.getWeek()][dayName];
 }
 
+/////////////
+
+//var d = new Date(); 
+
+var today = parseISOLocal('2017-07-24T19:40:00');
+
+var tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
 
 
+var mealsToday = mealsForDate(data,today);
+var mealsTomorrow = mealsForDate(data,tomorrow);
 
-// Browser
+console.log(mealsTomorrow);
+
 if(inBrowser) {
     window.onload = function() {
-        var meals = mealsForDate(data,d);
 
-        if(meals) {
-            var meal = data[year][week][dayName];
-            writeToClass(meals.Dinner,"dinnerToday");
-            writeToClass(meals.Vegetables, "vegetablesToday");
-            writeToClass(meals.Vegetarian, "vegetarianToday");
+        if(mealsToday) {
+            writeToClass(mealsToday.Dinner,"dinnerToday");
+            writeToClass(mealsToday.Vegetables, "vegetablesToday");
+            writeToClass(mealsToday.Vegetarian, "vegetarianToday");
         } else {
             document.getElementById('menu').style.display = 'none';
             document.getElementById('nomenu').style.display = 'block';
         }
+
+        if(mealsTomorrow) {
+            writeToClass(mealsTomorrow.Dinner,"dinnerTomorrow");
+            writeToClass(mealsTomorrow.Vegetables, "vegetablesTomorrow");
+            writeToClass(mealsTomorrow.Vegetarian, "vegetarianTomorrow");
+        } else {
+            document.getElementById('tomorrowdinner').style.display = 'none';
+        }
+
     }
 } else {
     // Not in browser
-    var meals = mealsForDate(data,d);
-    if(meals) {
-        console.log("Today:");
-        console.log("Dinner: " + meals.Dinner);
-        console.log("Vegetables: " + meals.Vegetables);
-        console.log("Vegetarian: " + meals.Vegetarian);
+
+   
+    if(mealsToday) {
+        console.log("\nToday:");
+        console.log("Dinner: " + mealsToday.Dinner);
+        console.log("Vegetables: " + mealsToday.Vegetables);
+        console.log("Vegetarian: " + mealsToday.Vegetarian);
     } else {
-        console.log("No menu exists");
+        console.log("No menu today.");
     }
+
+
+   if(mealsTomorrow) {
+        console.log("\nTomorrow:");
+        console.log("Dinner: " + mealsTomorrow.Dinner);
+        console.log("Vegetables: " + mealsTomorrow.Vegetables);
+        console.log("Vegetarian: " + mealsTomorrow.Vegetarian);
+    } else {
+        console.log("No menu tomorrow.");
+    }
+
+
 }
