092069552cbb26697751976ce2d67f0816d7fefc
Jake Coppinger <jake@jakecoppinger.com>
2017-07-13 16:15:47 +1000

    Added basic build script

--- build.sh
+++ build.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-browserify js/view.js -o js/bundle.js
+set -e
+
+npm test
+
+rm -rf dist/
+
+mkdir dist/
+mkdir dist/js
+mkdir dist/css
+
+cp index.html dist/
+cp robots.txt dist/
+cp CNAME dist/
+
+cp -r data/ dist/
+cp -r css/* dist/css/
+
+echo "Running browserify..."
+browserify js/view.js -o dist/js/bundle.js
+
+cp js/vue.min.js dist/js
+
+
+echo "Done!"
+
+
+
