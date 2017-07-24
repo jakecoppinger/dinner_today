5ae9c2db6ba302f8baca94c012217d72ba6ac165
Jake Coppinger <jake@jakecoppinger.com>
2017-07-24 20:21:46 +1000

    Implemented dinner finished message

--- src/js/view.js
+++ src/js/view.js
@@ -14,9 +14,21 @@ var humanDate = function(m) {
 var main = function() {
     //var today = moment("2017-07-24 09:30");
     var today = moment();
-
-
     var tomorrow = moment().add(1, 'days');
+
+    // Dinner finish time
+    const dinnerEnd = {
+        hours: 19,
+        minutes: 15
+    }
+
+    // Check if dinner has finished
+    var dinnerOver = false;
+    if(today.hours() >= dinnerEnd.hours &&
+        today.minutes() > dinnerEnd.minutes) {
+        dinnerOver = true;
+    }
+
     var dino = new DiningHall(data);
 
     var vueapp = new Vue({
@@ -27,7 +39,8 @@ var main = function() {
             mealsTomorrowData: dino.menuExistsOnDate(tomorrow),
             mealsToday: dino.mealsOnDate(today),
             mealsTomorrow: dino.mealsOnDate(tomorrow),
-            noMenu: !dino.menuExistsOnDate(today) && !dino.menuExistsOnDate(today)
+            noMenu: !dino.menuExistsOnDate(today) && !dino.menuExistsOnDate(today),
+            dinnerFinished: dinnerOver
         }
     });
 };
